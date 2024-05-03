b4c2e57e8f0d679c36dd62b325cb425e0940de90
EricDkr <xdsuperericxd@gmail.com>
2024-05-03 10:53:25 +0200

    signed commit test

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args){
         System.out.println("Hello");
-        System.out.println("3rd Test");
+        System.out.println("4rd Test");
 
     }
 }
