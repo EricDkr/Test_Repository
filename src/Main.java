ee995e67c7f659620e6f78eb89d7446e234455d2
EricDkr <xdsuperericxd@gmail.com>
2024-05-07 23:30:33 +0200

    Signed

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args){
         System.out.println("Hello");
         System.out.println("Signed again because i failed...");
+        System.out.println("Its nice to c u here ");
     }
 }
